b5d9195385f7e87c32b90e8b4daaf686118cffde
Pranav Hari <39328505+pHequals7@users.noreply.github.com>
2019-07-19 21:30:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,8 @@
 from flask import Flask,render_template,url_for,request
-import sys
 import re
 from scipy import spatial
 from operator import itemgetter
 import pandas as pd
-#import tensorflow as tf
-#import tensorflow_hub as hub
-from nltk import sent_tokenize
 import warnings
 import requests
 from bs4 import BeautifulSoup
@@ -43,29 +39,22 @@ def predict():
 				reco = reco+(imgurl,)
 				img_reco_list.append(reco)
 			else:
-				imgur = 'https://cutt.ly/EF53JZ'	
+				imgur = 'https://cutt.ly/EF53JZ'
 				reco = reco+(imgur,)
 				img_reco_list.append(reco)
 		return img_reco_list
 
 	def similar_movie(movie_name,topn=5):
-		sent_embed2 = movie.loc[movie_name,'embeddings']
-		# with tf.Graph().as_default():
-		# 	embed = hub.Module("./use/")
-		# 	messages = tf.placeholder(dtype=tf.string, shape=[None])
-		# 	output = embed(messages)
-		# 	with tf.Session() as session:
-		# 		session.run([tf.global_variables_initializer(), tf.tables_initializer()])
-		# 		sent_list = sent_tokenize(str(plot))
-		# 		clean_sent_list = clean_plot(sent_list)
-		#		sent_embed2 = (session.run(output, feed_dict={messages: clean_sent_list})).mean(axis=0).reshape(1,512)
-		similarities = []
-		for tensor,title,wikiurl in zip(movie['embeddings'],movie.index,movie['Wiki Page']):
-			cos_sim = 1 - spatial.distance.cosine(sent_embed2,tensor)
-			similarities.append((title,cos_sim,wikiurl))
-		#session.close()
-		reco_list = sorted(similarities,key=itemgetter(1),reverse=True)[1:topn+1]
-		return img_ins(reco_list)
+	    movie = pd.read_pickle('./movie_df.pkl')
+	    movie['Title_srch'] = movie['Title'].apply(lambda x: x.lower())
+	    movie.set_index('Title_srch', inplace=True)
+	    sent_embed2 = movie.loc[movie_name.lower(),'embeddings']
+	    similarities = []
+	    for tensor,title,wikiurl in zip(movie['embeddings'],movie['Title'],movie['Wiki Page']):
+		    cos_sim = 1 - spatial.distance.cosine(sent_embed2,tensor)
+		    similarities.append((title,cos_sim,wikiurl))
+	    reco_list = sorted(similarities,key=itemgetter(1),reverse=True)[1:topn+1]
+	    return img_ins(reco_list)
 
 	if request.method == 'POST':
 		movie_name = request.form['movie']
@@ -75,7 +64,5 @@ def predict():
 		return render_template('predict.html',prediction = my_prediction,moviename = data)
 
 if __name__ == '__main__':
-	movie = pd.read_pickle('./movie_df.pkl')
-	movie.set_index('Title', inplace=True)
-	app.run(debug=True)
+	app.run()
 
